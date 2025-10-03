e3fbf94702b644870f9a1ce240b1675c4f30e8c4
Akhmetshin Sergei <Sergei05Akhmetshin@yandex.ru>
2025-10-03 16:11:16 +0300

    хачу пва

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+import next_pwa from "next-pwa";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    //eslint: {
+        // ⚠️ Внимание: это позволяет сборке завершиться успешно даже при ошибках ESLint
+        //ignoreDuringBuilds: true,
+    //},
 };
 
 export default nextConfig;
+
+const withPWA = next_pwa({
+    dest: 'public', // сюда генерится service worker
+    register: true,
+    skipWaiting: true
+})
+
+module.exports = withPWA({
+    reactStrictMode: true
+})
+
